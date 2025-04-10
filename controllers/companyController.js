bde40c45fe9fe774ba6f898eac31523f49991192
Papia Bhowmik <bhowmikpapia7@gmail.com>
2025-04-10 22:26:16 +0530

    see company job commit

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -42,7 +42,27 @@ export const updateCompany = async (req, res) => {
 
 
 export const getCompanyJobs = async (req, res) => {
+  try{
+    const token = req.headers.authorization?.split(" ")[1];
+      if (!token) {
+        return res.status(401).json({ message: "Unauthorized Token" });
+      }
   
+      const decode = verifyToken(token);
+      if (!decode) {
+        return res.status(401).json({ message: "Token Invalid" });
+      }
+  
+      if (decode.user_type !== 2) {
+        return res.status(403).json({ message: "Forbidden: Only company can view their job" });
+      }
+      const jobs = await Job.find({companId: decode.id});
+      return res.status(200).json({jobs});
+  }catch(error){
+    console.error("Updated Error:", error);
+    res.status(500).json({ message: "Server error. Please try again later." });
+    
+  }
 };
 
 
