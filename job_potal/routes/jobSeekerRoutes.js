5bc9d6a90de88826c600895c41d6dde887d742aa
Papia Bhowmik <128517978+papiabhowmik@users.noreply.github.com>
2025-05-02 10:45:57 +0530

    Delete job_potal directory

--- job_potal/routes/jobSeekerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { updateJobSeeker } from "../controllers/jobSeekerController.js";
-
-const router = express.Router();
-
-router.post("/update-jobseeker", updateJobSeeker);
-// router.put("/update-jobseeker/:jobseeker_id", updateJobSeeker);
-
-export default router;
